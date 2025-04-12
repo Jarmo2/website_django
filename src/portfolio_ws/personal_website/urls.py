c80f2d14bd74634708584aff6075441dcb2c0bc5
Jarmo Kruse <jarmo2.0@tutanota.com>
2025-04-12 11:07:47 +0200

    starting filling projects and adjusting template

--- src/portfolio_ws/personal_website/urls.py
+++ src/portfolio_ws/personal_website/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 from personal_website import views
 
 urlpatterns = [
-    path('/', views.start_page)
+    path('', views.start_page, name="home"),
+    path('about', views.about_page, name='about')
     ]
--- src/portfolio_ws/personal_website/views.py
+++ src/portfolio_ws/personal_website/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 def start_page(request):
-    return render(request, "personal_website/index.html")
+    projects = Project.objects.all()
+    return render(request, "personal_website/base.html", {'projects': projects})
+
+def about_page(request):
+    return render(request, "personal_website/about_extension.html")
